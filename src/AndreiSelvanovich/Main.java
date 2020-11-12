2809b509b5762baf74d7f5cea16d70d4fe8d9af0
Andrei Selvanovich <vasilich85@gmail.com>
2020-11-12 10:09:58 -0800

    new System.out1

--- src/AndreiSelvanovich/Main.java
+++ src/AndreiSelvanovich/Main.java
@@ -3,5 +3,7 @@ package AndreiSelvanovich;
 public class Main {
     public static void main(String[] args) {
         System.out.println("How are you?");
+
+        System.out.println("New branch");
     }
 }
